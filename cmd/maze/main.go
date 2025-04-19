c005d73221edf8184c095ab5800ea1961e703c62
jcraver <25520616+jcraver1021@users.noreply.github.com>
2025-04-18 23:42:58 -0400

    Adds flags for command line invocation

--- cmd/maze/main.go
+++ cmd/maze/main.go
@@ -1,18 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"tsumegolang/internal/maze"
 )
 
+var (
+	widthFlag    = flag.Int("width", 20, "Width of the maze")
+	heightFlag   = flag.Int("height", 20, "Height of the maze")
+	filenameFlag = flag.String("filename", "maze.png", "Output filename for the maze image")
+)
+
 func main() {
-	width := 20
-	height := 20
-	err := maze.GenerateMaze(width, height, "maze.png")
+	flag.Parse()
+
+	err := maze.GenerateMaze(*widthFlag, *heightFlag, *filenameFlag)
 	if err != nil {
 		fmt.Printf("Error generating maze: %v\n", err)
 		return
 	}
-	fmt.Printf("Maze generated with dimensions %dx%d\n", width, height)
+	fmt.Printf("Maze generated successfully: %s (%dx%d)\n", *filenameFlag, *widthFlag, *heightFlag)
 }
